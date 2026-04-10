04651fbe3c40f85a47b4053651e62e2da8027f97
Breno Paim Targa <brenotarga@proton.me>
2026-04-10 17:39:24 -0300

    updated logic to include api city name

--- functions.py
+++ functions.py
@@ -1,31 +1,34 @@
 import requests
+from geopy.exc import GeocoderTimedOut, GeocoderUnavailable
 from geopy.geocoders import Nominatim
-from geopy.exc import GeocoderUnavailable, GeocoderTimedOut
+
 
 def Get_Weather(city):
     try:
-        coords = Get_Coords(city)
+        cityData = Get_Data(city)
     except (GeocoderUnavailable, GeocoderTimedOut):
         return "Não foi possível estabelecer conexão."
     except InvalidLocation:
         return "Localização inválida."
 
-    lat = coords["latitude"]
-    lon = coords["longitude"]
+    lat = cityData["latitude"]
+    lon = cityData["longitude"]
+    name = cityData["name"]
 
     try:
-        data = Fetch_Api(Create_Url(lat,lon))
+        data = Fetch_Api(Create_Url(lat, lon))
     except requests.exceptions.RequestException:
         return "Erro ao receber dados."
 
     temp = data["current"]["temperature_2m"]
 
-    return  f"Temperatura atual em {city}: {temp}°C {Get_Emoji(temp)}"
+    return f"Temperatura atual em {name}: {temp}°C {Get_Emoji(temp)}"
 
-def Get_Coords(city):
+
+def Get_Data(city):
     if not city.strip():
         raise InvalidLocation
-    
+
     geolocator = Nominatim(user_agent="meee")
 
     try:
@@ -35,16 +38,25 @@ def Get_Coords(city):
 
     if not location:
         raise InvalidLocation
-    
-    return {"latitude": location.latitude, "longitude": location.longitude}
-    
-def Create_Url(lat,lon):
+
+    locationDict = location.raw
+
+    return {
+        "latitude": locationDict["lat"],
+        "longitude": locationDict["lon"],
+        "name": locationDict["name"],
+    }
+
+
+def Create_Url(lat, lon):
     return f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m"
+
 
 def Fetch_Api(url):
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
+
 
 def Get_Emoji(temp):
     match temp:
@@ -54,6 +66,7 @@ def Get_Emoji(temp):
             return "😎"
         case _:
             return "🥵"
+
 
 class InvalidLocation(Exception):
     pass
